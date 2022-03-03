269604d61a499de77b3fca46b784a6152c00a40c
Nklj <vlkvnklj@gmail.com>
2022-03-03 10:15:45 +0300

    add files from other branches

---
+++ randomMoove.js
@@ -0,0 +1,60 @@
+const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const c = canvas.getContext("2d");
+
+class Circle {
+  constructor(x, y, radius, dx, dy, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = this.color;
+    //c.fillStyle = this.color;
+    //c.fill();
+    c.stroke();
+    this.update();
+  }
+  update() {
+    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.dx = (Math.random() - 0.5) * 17;
+    this.dy = (Math.random() - 0.5) * 17;
+  }
+}
+
+const circlesArray = [];
+
+for (let i = 0; i < 1000; i++) {
+  let radius = Math.random() * 5;
+  let x = Math.random() * (window.innerWidth - 2 * radius) + radius;
+  let y = Math.random() * (window.innerHeight - 2 * radius) + radius;
+  let dx = (Math.random() - 0.5) * 3;
+  let dy = (Math.random() - 0.5) * 3;
+  const random16 = () => Math.floor(Math.random() * 16 * 16).toString(16);
+  let color = `#${random16()}${random16()}${random16()}`;
+  circlesArray.push(new Circle(x, y, radius, dx, dy, color));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  //c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  for (let i = 0; i < circlesArray.length; i++) {
+    circlesArray[i].draw();
+  }
+}
+
+animate();
