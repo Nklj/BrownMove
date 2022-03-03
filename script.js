fdbe591f0cb801cb7ec7f977f0e3f2317f58a6c6
Nklj <vlkvnklj@gmail.com>
2022-03-03 22:12:48 +0300

    add sidebar

--- script.js
+++ script.js
@@ -1,58 +1,97 @@
+import { settings } from "./settings.js";
+
+const versionButtons = document.querySelector(".versionButtons");
 const canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
+const W = window.innerWidth;
+const H = window.innerHeight;
+let animateId = null;
+
+canvas.width = W;
+canvas.height = H;
 const c = canvas.getContext("2d");
 
 class Circle {
-  constructor(x, y, radius, dx, dy, color) {
+  constructor(x, y, rad, dx, dy, color, colorBorder, move) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+    this.rad = rad;
     this.dx = dx;
     this.dy = dy;
     this.color = color;
+    this.colorBorder = colorBorder;
+    this.move = move;
+    this.memoDx = this.dx;
+    this.memoDy = this.dy;
   }
+
   draw() {
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = "green";
+    c.arc(this.x, this.y, this.rad, 0, Math.PI * 2, false);
+    c.strokeStyle = this.colorBorder;
     c.fillStyle = this.color;
     c.fill();
     c.stroke();
     this.update();
   }
   update() {
-    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
-      this.dx = -this.dx;
-    }
-    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
-      this.dy = -this.dy;
-    }
+    if (this.x + this.rad > W || this.x - this.rad < 0) this.dx = -this.dx;
+    if (this.y + this.rad > H || this.y - this.rad < 0) this.dy = -this.dy;
     this.x += this.dx;
     this.y += this.dy;
+    if (this.move === "random") {
+      this.dx = (Math.random() - 0.5) * this.memoDx;
+      this.dy = (Math.random() - 0.5) * this.memoDy;
+    }
   }
 }
 
-const circlesArray = [];
+const versionHandler = (e) => {
+  cancelAnimationFrame(animateId);
+  const data = settings[e.target.id - 1];
+  document.body.style.backgroundColor = data.background;
 
-for (let i = 0; i < 1000; i++) {
-  let radius = Math.random() * 15;
-  let x = Math.random() * (window.innerWidth - 2 * radius) + radius;
-  let y = Math.random() * (window.innerHeight - 2 * radius) + radius;
-  let dx = (Math.random() - 0.5) * 3;
-  let dy = (Math.random() - 0.5) * 15;
-  const random16 = () => Math.floor(Math.random() * 16 * 16).toString(16);
-  let color = `#${random16()}${random16()}${random16()}`;
-  circlesArray.push(new Circle(x, y, radius, dx, dy, color));
-}
+  const circlesArray = [];
 
-function animate() {
-  requestAnimationFrame(animate);
-
-  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  for (let i = 0; i < circlesArray.length; i++) {
-    circlesArray[i].draw();
+  for (let i = 0; i < data.numbers; i++) {
+    const rand = () => Math.random();
+    let rad = rand() * data.rad;
+    let x = rand() * (W - 2 * rad) + rad;
+    let y = rand() * (H - 2 * rad) + rad;
+    let dx = (rand() - 0.5) * data.dx;
+    let dy = (rand() - 0.5) * data.dy;
+    const hex = () => Math.floor(rand() * 16 * 16).toString(16);
+    let color =
+      data.color === "random" ? `#${hex()}${hex()}${hex()}` : data.color;
+    let colorBorder =
+      data.colorBorder === "random"
+        ? `#${hex()}${hex()}${hex()}`
+        : data.colorBorder;
+    circlesArray.push(
+      new Circle(x, y, rad, dx, dy, color, colorBorder, data.move)
+    );
   }
+
+  function animate() {
+    data.trace ? null : c.clearRect(0, 0, W, H);
+    for (let i = 0; i < circlesArray.length; i++) {
+      circlesArray[i].draw();
+    }
+    animateId = requestAnimationFrame(animate);
+  }
+
+  animate();
+};
+
+function render() {
+  const buttons = settings.map((item) => {
+    const button = document.createElement("button");
+    button.addEventListener("click", versionHandler);
+    button.innerText = `Version ${item.id}`;
+    button.id = item.id;
+    return button;
+  });
+  versionButtons.replaceChildren(...buttons);
 }
 
-animate();
+render();
